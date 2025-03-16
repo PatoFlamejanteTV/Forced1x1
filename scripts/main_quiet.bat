bba6cb34be6dd241edd1eab530b4cace2aa3af71
UltimateQuack <patoflamejantetv@gmail.com>
2025-03-16 16:44:36 -0300

    quiet VERSIONgit add .!
    quiet does NOT displays the 'processing x.png...' messages anymore!

---
+++ scripts/main_quiet.bat
@@ -0,0 +1,20 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: Check for ImageMagick installation
+where convert >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Error: ImageMagick is required but not found in PATH.
+    echo Install it from https://imagemagick.org/ and make sure convert.exe is available
+    exit /b 1
+)
+
+:: Process images in all subdirectories
+for /r %%F in (
+    *.png *.jpg *.jpeg *.gif *.bmp *.tiff *.PNG *.JPG *.JPEG *.GIF *.BMP *.TIFF
+) do (
+    convert "%%~fF" -resize 1x1! "%%~fF"
+)
+
+echo All images have been resized to 1x1 pixels.
+endlocal
